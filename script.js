d6aaae5d2ec7444a6da254279cbe47c77138dfcd
Carlos Lucas Sales de Andrade <132520068+lucassale@users.noreply.github.com>
2024-06-02 09:52:13 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Desenvolva sua lógica aqui
+const button = document.querySelector('.main__add')
+const main = document.querySelector('main')
+
+button.addEventListener('click', () => {
+    const input = document.querySelector('#task')
+    const ul = document.createElement('ul')
+    ul.classList.add("main__list")
+    const li = document.createElement('li')
+    const excluir = document.createElement('button')
+    li.innerText = input.value
+    main.append(ul, li, excluir)
+})
+
